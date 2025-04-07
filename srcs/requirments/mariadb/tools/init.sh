1cf6887767e0755172877963001c215fe85bd26f
Nolan Diane <noldiane@c1r8p3.42nice.fr>
2025-04-07 12:28:23 +0200

    added

--- srcs/requirments/mariadb/tools/init.sh
+++ srcs/requirments/mariadb/tools/init.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-echo "Setting up database ${SQL_DATABASE} and user ${SQL_USER}..."
+echo "Setting up database ${DB_NAME} and user ${DB_USER}..."
 
 # INIT MYSQL #
 
 service mariadb start;
 sleep 0.5
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 
 # CREATING ADMIN USER #
 
-mysql -e "CREATE USER '${SQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_ROOT_USER}'@'localhost' WITH GRANT OPTION;"
+mysql -e "SELECT 1 FROM mysql.user WHERE user='${DB_USER}' AND host='%' LIMIT 1;" | grep -q 1 || \
+mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
+# GRANT PERMISSIONS
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
 
 # CREATING NORMAL USER #
 
-mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -e "SELECT 1 FROM mysql.user WHERE user='${SQL_USER}' AND host='localhost';" | grep -q 1 || \
+#mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysqladmin -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -p$DB_PASSWORD shutdown
 exec mysqld_safe
